9907dc752745d088d743d49e19601c88338058e1
cpcorvers <cp.corvers@disroot.org>
2023-02-07 21:18:16 +0100

    local & server the same, server in docs

--- server-make.py
+++ server-make.py
@@ -4,14 +4,12 @@ import glob
 import datetime
 import operator
 from pathlib import Path
+import shutil
+# import os
 
-# (un)comment before uploading to web
-# outputfile = 'output'
-# site = {'title': 'local cp.corvers',
-#         'url': '../output',
-#         'home': 'index.html',
-#         'pathtoimages': '../../content/images/', }
-site = {'title': 'cp.corvers', 'url': 'https://www.cpcorvers.nl'}
+site = {'title': 'cp.corvers',
+        'url': 'https://www.cpcorvers.nl',
+        'pathtoimages': '../images/'}
 outputfile = 'docs'
 
 # load yml datafiles from /content/data
@@ -43,10 +41,34 @@ for file in datafiles_work:
 # sorted() the items in worklist so they are presented in the intended order
 worklist = sorted(worklist, key=operator.itemgetter('modal-id'))
 
-# for work in worklist:
-#     print(work['title'], work['modal-id'])
-#
-# render pages fr m templates
+# copy imagery to the docs directory for upload to server
+# define source and destination directory
+src_dir = './content/images/'
+dst_dir = Path(f'./{outputfile}/images/')
+# create the destination directory if it doesn't exist
+Path(dst_dir).mkdir(exist_ok=True, parents=True)
+# get list of all files in source directory with the glob.py library
+# by setting the root_dir to source directory glob only returns filenames
+imagery = glob.glob('*.*', root_dir=src_dir)
+# copy all files in the source directory to the destination using shutil.py
+for image in imagery:
+    src_file = Path(f"{src_dir}{image}")
+    dst_file = Path(f"{dst_dir}/{image}")
+    shutil.copy2(src_file, dst_file)
+
+# copy css files to the docs directory for upload to server
+# define source and destination directory
+src_dir = './theme/css/'
+dst_dir = Path(f'./{outputfile}/')
+# create the destination directory if it doesn't exist
+stylesheet = glob.glob('*.css', root_dir=src_dir)
+# copy all files in the source directory to the destination using shutil.py
+for css in stylesheet:
+    src_file = Path(f"{src_dir}{css}")
+    dst_file = Path(f"{dst_dir}/{css}")
+    shutil.copy2(src_file, dst_file)
+
+# render pages from templates
 # and write html to a file
 fileloader = FileSystemLoader("theme/templates")
 env = Environment(loader=fileloader)
