4d1957a2457d3e58c05e3e287816c339d7033c51
cpcorvers <cp.corvers@disroot.org>
2023-03-11 11:46:42 +0100

    en/nl languages and vakmakerij added

--- server-make.py
+++ server-make.py
@@ -9,10 +9,17 @@ import shutil
 
 site = {'title': 'cp.corvers',
         'url': 'https://www.cpcorvers.nl',
-        'pathtoimages': '../images/'}
+        'home': 'index.html',
+        'pathtoimages': '../../images/'}
 outputfile = 'docs'
+language = ['en', 'nl']
+pages = ['index', 'vision', 'techniques', 'workportfolio',
+         'workshops', 'tools', 'about', 'vakmakerij']
 
 # load yml datafiles from /content/data
+# datafiles = ['workshops', 'navigation', 'testimonials', 'techniques', 'techniques_intro',
+# 'tools', 'tools_intro', 'about', 'about_intro', 'vision_intro', 'vakmakerij', 'workshop_intro']
+
 with open("content/data/workshops.yml") as f:
     yaml = YAML(typ="safe", pure=True)
     workshops = yaml.load(f)
@@ -25,9 +32,41 @@ with open("content/data/testimonials.yml") as h:
     yaml = YAML(typ="safe", pure=True)
     testimonials = yaml.load(h)
 
+with open("content/data/techniques.yml") as j:
+    yaml = YAML(typ="safe", pure=True)
+    techniques = yaml.load(j)
+
+with open("content/data/techniques_intro.yml") as m:
+    yaml = YAML(typ="safe", pure=True)
+    techniques_intro = yaml.load(m)
+
+with open("content/data/tools.yml") as k:
+    yaml = YAML(typ="safe", pure=True)
+    tools = yaml.load(k)
+
+with open("content/data/tools_intro.yml") as k:
+    yaml = YAML(typ="safe", pure=True)
+    tools_intro = yaml.load(k)
+
 with open("content/data/about.yml") as i:
     yaml = YAML(typ="safe", pure=True)
     about = yaml.load(i)
+
+with open("content/data/about_intro.yml") as i:
+    yaml = YAML(typ="safe", pure=True)
+    about_intro = yaml.load(i)
+
+with open("content/data/vision_intro.yml") as n:
+    yaml = YAML(typ="safe", pure=True)
+    vision_intro = yaml.load(n)
+
+with open("content/data/vakmakerij.yml") as o:
+    yaml = YAML(typ="safe", pure=True)
+    vakmakerij = yaml.load(o)
+
+with open("content/data/workshop_intro.yml") as p:
+    yaml = YAML(typ="safe", pure=True)
+    workshops_intro = yaml.load(p)
 
 # load yml datafiles from content/work
 worklist = []
@@ -76,89 +115,49 @@ for css in stylesheet:
 fileloader = FileSystemLoader("theme/templates")
 env = Environment(loader=fileloader)
 
-page = 'index'
-rendered = env.get_template(f'{page}.html').render(
-    navigation=navigation,
-    pagetitle=page,
-    site=site)
-fileName = Path(f"./{outputfile}/{page}.html")
-fileName.parent.mkdir(exist_ok=True, parents=True)
-with open(f"{fileName}", "w") as f:
-    f.write(rendered)
+for lang in language:
+    for page in pages:
+        if page == 'index':
+            filename = f'/{page}.html'
+        else:
+            filename = f'/{page}/index.html'
+        filelocation = Path(f"{outputfile}/{lang}/{filename}")
+        rendered = env.get_template(f'{page}.html').render(
+            navigation=navigation,
+            pagetitle=page,
+            lang=lang,
+            pageurl=filename,
+            techniques=techniques,
+            techniques_intro=techniques_intro,
+            worklist=worklist,
+            workshops_intro=workshops_intro,
+            workshops=workshops,
+            tools=tools,
+            tools_intro=tools_intro,
+            about=about,
+            about_intro=about_intro,
+            vision_intro=vision_intro,
+            testimonials=testimonials,
+            vakmakerij=vakmakerij,
+            site=site)
+        filelocation.parent.mkdir(exist_ok=True, parents=True)
+        with open(f"{filelocation}", "w") as f:
+            f.write(rendered)
 
-page = 'vision'
-rendered = env.get_template(f'{page}.html').render(
-    navigation=navigation,
-    pagetitle=page,
-    site=site)
-fileName = Path(f"./{outputfile}/{page}/index.html")
-fileName.parent.mkdir(exist_ok=True, parents=True)
-with open(f"{fileName}", "w") as f:
-    f.write(rendered)
-
-page = ('workportfolio', 'work')
-rendered = env.get_template(f'{page[0]}.html').render(
-    navigation=navigation,
-    worklist=worklist,
-    pagetitle=page[1],
-    site=site)
-fileName = Path(f"./{outputfile}/{page[1]}/index.html")
-fileName.parent.mkdir(exist_ok=True, parents=True)
-with open(f"{fileName}", "w") as f:
-    f.write(rendered)
-
-for work in worklist:
-    page = 'workpost'
-    rendered = env.get_template(f'{page}.html').render(
-        navigation=navigation,
-        work=work,
-        site=site)
-    fileName = Path(f"./{outputfile}/{work['pagetitle']}/index.html")
-    fileName.parent.mkdir(exist_ok=True, parents=True)
-    with open(f"{fileName}", "w") as f:
-        f.write(rendered)
-
-page = 'techniques'
-rendered = env.get_template(f'{page}.html').render(
-    navigation=navigation,
-    pagetitle=page,
-    site=site)
-fileName = Path(f"./{outputfile}/{page}/index.html")
-fileName.parent.mkdir(exist_ok=True, parents=True)
-with open(f"{fileName}", "w") as f:
-    f.write(rendered)
-
-page = 'workshops'
-rendered = env.get_template(f'{page}.html').render(
-    workshops=workshops,
-    navigation=navigation,
-    pagetitle=page,
-    site=site)
-fileName = Path(f"./{outputfile}/{page}/index.html")
-fileName.parent.mkdir(exist_ok=True, parents=True)
-with open(f"{fileName}", "w") as f:
-    f.write(rendered)
-
-page = 'tools'
-rendered = env.get_template(f'{page}.html').render(
-    navigation=navigation,
-    pagetitle=page,
-    site=site)
-fileName = Path(f"./{outputfile}/{page}/index.html")
-fileName.parent.mkdir(exist_ok=True, parents=True)
-with open(f"{fileName}", "w") as f:
-    f.write(rendered)
-
-page = 'about'
-rendered = env.get_template(f'{page}.html').render(
-    about=about,
-    testimonials=testimonials,
-    navigation=navigation,
-    pagetitle=page,
-    site=site)
-fileName = Path(f"./{outputfile}/{page}/index.html")
-fileName.parent.mkdir(exist_ok=True, parents=True)
-with open(f"{fileName}", "w") as f:
-    f.write(rendered)
+for lang in language:
+    for work in worklist:
+        page = 'workpost'
+        filename = f"/{work['pagetitle']}/index.html"
+        filelocation = Path(
+            f"{outputfile}/{lang}/{work['pagetitle']}/index.html")
+        rendered = env.get_template(f'{page}.html').render(
+            navigation=navigation,
+            lang=lang,
+            pageurl=filename,
+            work=work,
+            site=site)
+        filelocation.parent.mkdir(exist_ok=True, parents=True)
+        with open(f"{filelocation}", "w") as f:
+            f.write(rendered)
 
 print('rendered: ', datetime.datetime.now())
