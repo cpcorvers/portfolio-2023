374dd18458349c6436e0676ca0e5da486c0c1fa3
cpcorvers <cp.corvers@disroot.org>
2023-03-14 19:04:47 +0100

    technique links

--- server-make.py
+++ server-make.py
@@ -80,36 +80,6 @@ for file in datafiles_work:
 # sorted() the items in worklist so they are presented in the intended order
 worklist = sorted(worklist, key=operator.itemgetter('modal-id'))
 
-# copy imagery to the docs directory for upload to server
-# define source and destination directory
-src_dir = './content/images/'
-dst_dir = Path(f'./{outputfile}/images/')
-# create the destination directory if it doesn't exist
-Path(dst_dir).mkdir(exist_ok=True, parents=True)
-# get list of all files in source directory with the glob.py library
-# by setting the root_dir to source directory glob only returns filenames
-imagery = glob.glob('*.*', root_dir=src_dir)
-# copy all files in the source directory to the destination using shutil.py
-for image in imagery:
-    src_file = Path(f"{src_dir}{image}")
-    dst_file = Path(f"{dst_dir}/{image}")
-    shutil.copy2(src_file, dst_file)
-
-# copy css files to the docs directory for upload to server
-# define source and destination directory
-src_dir = './theme/css/'
-dst_dir = Path(f'./{outputfile}/')
-# create the destination directory if it doesn't exist
-Path(dst_dir).mkdir(exist_ok=True, parents=True)
-# get list of all files in source directory with the glob.py library
-# by setting the root_dir to source directory glob only returns filenames
-stylesheet = glob.glob('*.css', root_dir=src_dir)
-# copy all files in the source directory to the destination using shutil.py
-for css in stylesheet:
-    src_file = Path(f"{src_dir}{css}")
-    dst_file = Path(f"{dst_dir}/{css}")
-    shutil.copy2(src_file, dst_file)
-
 # render pages from templates
 # and write html to a file
 fileloader = FileSystemLoader("theme/templates")
@@ -159,5 +129,50 @@ for lang in language:
         filelocation.parent.mkdir(exist_ok=True, parents=True)
         with open(f"{filelocation}", "w") as f:
             f.write(rendered)
+
+# copy imagery to the docs directory for upload to server
+# define source and destination directory
+src_dir = './content/images/'
+dst_dir = Path(f'./{outputfile}/images/')
+# create the destination directory if it doesn't exist
+Path(dst_dir).mkdir(exist_ok=True, parents=True)
+# get list of all files in source directory with the glob.py library
+# by setting the root_dir to source directory glob only returns filenames
+imagery = glob.glob('*.*', root_dir=src_dir)
+# copy all files in the source directory to the destination using shutil.py
+for image in imagery:
+    src_file = Path(f"{src_dir}{image}")
+    dst_file = Path(f"{dst_dir}/{image}")
+    shutil.copy2(src_file, dst_file)
+
+# copy css files to the docs directory for upload to server
+# define source and destination directory
+src_dir = './theme/css/'
+dst_dir = Path(f'./{outputfile}/')
+# create the destination directory if it doesn't exist
+Path(dst_dir).mkdir(exist_ok=True, parents=True)
+# get list of all files in source directory with the glob.py library
+# by setting the root_dir to source directory glob only returns filenames
+stylesheet = glob.glob('*.css', root_dir=src_dir)
+# copy all files in the source directory to the destination using shutil.py
+for css in stylesheet:
+    src_file = Path(f"{src_dir}{css}")
+    dst_file = Path(f"{dst_dir}/{css}")
+    shutil.copy2(src_file, dst_file)
+
+# copy index to docs/index as landingpage
+# define source and destination directory
+src_dir = './docs/nl/'
+dst_dir = './docs/'
+# create the destination directory if it doesn't exist
+# Path(dst_dir).mkdir(exist_ok=True, parents=True)
+# get list of all files in source directory with the glob.py library
+# by setting the root_dir to source directory glob only returns filenames
+stylesheet = glob.glob('index.html', root_dir=src_dir)
+# copy all files in the source directory to the destination using shutil.py
+for index in stylesheet:
+    src_file = Path(f"{src_dir}{index}")
+    dst_file = Path(f"{dst_dir}/{index}")
+    shutil.copy2(src_file, dst_file)
 
 print('rendered: ', datetime.datetime.now())
